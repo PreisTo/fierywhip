3ecb5cd0be80c7d42ae7851dc174b0e3a14a372c
Tobias Preis <tobias.preis@tum.de>
2023-10-04 13:18:08 +0200

    table prep and triggerID2trigger

--- recreate_berlato/localize_trigdat.py
+++ recreate_berlato/localize_trigdat.py
@@ -6,6 +6,7 @@ import urllib.request
 import os
 from gbmbkgpy.io.downloading import download_trigdata_file
 from glob import glob
+import pandas as pd
 
 
 def run_trigger(trigger):
@@ -38,5 +39,36 @@ def run_trigger(trigger):
     os.remove("temp.json")
 
 
+def table_prep():
+    table = pd.read_csv("table.csv", sep="\t", decimal=".")
+    return list(table["Trigger"])
+
+
+def triggerID2trigger(triggerid):
+    url = f"https://gcn.gsfc.nasa.gov/other/{triggerid}.fermi"
+    with urllib.request.urlopen(url) as u:
+        content = u.read()
+        content = content.decode()
+    grb_date_id = content.find("GRB_DATE")
+    grb_time_id = content.find("GRB_TIME")
+    sod_id = content.find("SOD")
+    grb_date = content[grb_date_id:grb_time_id].split("\n")
+    grb_date = grb_date[0][-8::]
+    grb_date = grb_date[0:2] + grb_date[3:5] + grb_date[6:]
+    grb_time = content[grb_time_id:sod_id]
+    grb_time = grb_time.strip("GRB_TIME:").strip(" ").strip("\t")
+    tot_seconds = 24 * 3600
+    grb_time = float(grb_time)
+    t = grb_time / tot_seconds
+    t = str(t).split(".")
+    trigger = f"GRB{grb_date}{t[1][:3]}"
+    print(trigger)
+    return trigger
+
+
 if __name__ == "__main__":
-    run_trigger("GRB230913408")
+    # run_trigger("GRB230913408")
+    triggers = table_prep()
+    for t in triggers:
+        trigger = triggerID2trigger(t)
+        run_trigger(trigger)
