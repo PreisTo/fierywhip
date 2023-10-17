c3e76ae3f54d99d47bac2924ff6824f3a45cc2fa
PreisTo <tpreis@mpe.mpg.de>
2023-10-17 11:20:09 +0200

    energy scatter plot

--- eff_area/src/effarea/io/plotting.py
+++ eff_area/src/effarea/io/plotting.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import numpy as np
 import yaml
 from effarea.utils.detectors import detector_list, nai_list, name_to_id
@@ -17,7 +18,6 @@ class Plots:
 
     def from_result_yaml(self, yaml_path, base_dir=None):
         """ """
-        self.__init__()
         if base_dir is None:
             self._base_dir = os.path.join(
                 os.environ.get("GBMDATA"), "localizing/result_plots/"
@@ -57,38 +57,135 @@ class Plots:
                     energies_dict[e][1].append(norm)
         return energies_dict
 
-    def energy_scatter_plot(self, energies=None, det=None):
+    def energy_scatter_plot(self, energies=None, det=None, path=None):
         if det is None:
             det = nai
         else:
             assert type(det) == list, "Det must be a list"
         if energies is None:
             energies = self._energies
+            try:
+                energies.pop(energies.index("8.1-700"))
+            except IndexError:
+                pass
+            starting_energies = []
+            for i, e in enumerate(energies):
+                starting_energies.append(float(e.split("-")[0]))
+            sorting_indices = np.argsort(np.array(starting_energies))
+            sorted_energies = []
+            for i in sorting_indices:
+                sorted_energies.append(energies[i])
+            energies = sorted_energies
         else:
             assert type(energies) == list, "Energies must be list"
+        if path is None:
+            plot_path = os.path.join(self._base_dir, "dets")
+        else:
+            plot_path = path
 
         for d in det:
-            plot_path = os.path.join(self._base_dir)
+            num_plots = len(energies)
+
             energy_dict = self.detectors_array[name_to_id(d)]
             if not os.path.exists(plot_path):
                 os.makedirs(plot_path)
-            num_axes = math.ceil(len(energies) / 3)
+            num_axes = math.ceil(len(energies) / num_plots)
             fig, axes = plt.subplots(num_axes)
+            if type(axes) != list:
+                axes = [axes]
             counter = 0
             ax = 0
-            colors = ["red", "blue", "green"]
+            colors = list(mcolors.TABLEAU_COLORS.values())
             for e in energies:
-                if counter < 3:
-                    axes[ax].scatter(
-                        energy_dict[e][0],
-                        energy_dict[e][1],
+                e_start = round(float(e.split("-")[0]), 3)
+                e_stop = round(float(e.split("-")[-1]), 3)
+
+                if counter < num_plots:
+                    plot_data = np.sort(energy_dict[e][0:2], axis=1)
+                    axes[ax].plot(
+                        plot_data[0],
+                        plot_data[1],
+                        marker="+",
+                        linestyle="--",
                         color=colors[counter],
-                        label=f"Bins {e}",
+                        label=f"Energy {e_start}-{e_stop}",
                     )
                     counter += 1
                 else:
                     ax += 1
                     counter = 0
-            axes[0].set
+                    plot_data = np.sort(energy_dict[e][0:2], axis=1)
+                    axes[ax].plot(
+                        plot_data[0],
+                        plot_data[1],
+                        marker="+",
+                        linestyle="--",
+                        color=colors[counter],
+                        label=f"Energy {e_start}-{e_stop}",
+                    )
+                    counter += 1
+            for ax in axes:
+                ax.legend()
+
             fig.tight_layout()
-            fig.savefig(os.path.join(plot_path, f"{d}_.pdf"))
+            fig.savefig(os.path.join(plot_path, f"{d}.pdf"))
+            plt.close(fig)
+
+    def det_scatter_plot(self, plot_path=None, dets=None):
+        if plot_path is None:
+            plot_path = os.path.join(self._base_dir, "energies")
+        if dets is None:
+            dets = nai_list()
+        for e in self._energies:
+            num_plots = 6
+
+            if not os.path.exists(plot_path):
+                os.makedirs(plot_path)
+            num_axes = math.ceil(len(dets) / num_plots)
+            fig, axes = plt.subplots(num_axes, figsize=(5, 7), sharex=True)
+            if type(axes) not in (list, np.ndarray):
+                axes = [axes]
+            counter = 0
+            ax = 0
+            colors = list(mcolors.TABLEAU_COLORS.values())
+            sides = [
+                ["n0", "n1", "n2", "n3", "n4", "n5"],
+                ["n6", "n7", "n8", "n9", "na", "nb"],
+            ]
+            for s in sides:
+                for d in s:
+                    energy_dict = self.detectors_array[name_to_id(d)]
+                    if counter < num_plots:
+                        plot_data = np.sort(energy_dict[e][0:2], axis=1)
+                        axes[ax].plot(
+                            plot_data[0],
+                            plot_data[1],
+                            marker="+",
+                            linestyle="--",
+                            color=colors[counter],
+                            label=f"Det {d}",
+                        )
+                        counter += 1
+                    else:
+                        ax += 1
+                        counter = 0
+                        plot_data = np.sort(energy_dict[e][0:2], axis=1)
+                        axes[ax].plot(
+                            plot_data[0],
+                            plot_data[1],
+                            marker="+",
+                            linestyle="--",
+                            color=colors[counter],
+                            label=f"Det {d}",
+                        )
+                        counter += 1
+            for ax in axes:
+                ax.legend()
+            e_start = round(float(e.split("-")[0]), 3)
+            e_stop = round(float(e.split("-")[-1]), 3)
+
+            axes[0].set_title("b0 side")
+            axes[1].set_title("b1 side")
+            fig.tight_layout()
+            fig.savefig(os.path.join(plot_path, f"{e_start}-{e_stop}.pdf"))
+            plt.close(fig)
