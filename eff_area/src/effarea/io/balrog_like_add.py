c8bbd2093d8884b776d9332a13f84c6e3a2162c6
PreisTo <tpreis@mpe.mpg.de>
2023-10-24 14:56:36 +0200

    init BALROGLike with pos prior

---
+++ eff_area/src/effarea/io/balrog_like_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from gbm_drm_gen.io.balrog_like import BALROGLike
+
+
+class BALROGLikePositionPrior(BALROGLike):
+    def __init__(self):
+        return None
+
+    def set_model(self, likelihood_model):
+        return None
