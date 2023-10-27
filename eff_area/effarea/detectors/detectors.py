ea34aaf26518547a442ae2a1412c45f877f5fd68
PreisTo <tpreis@mpe.mpg.de>
2023-10-27 11:06:40 +0200

    set gbm_frame

--- eff_area/effarea/detectors/detectors.py
+++ eff_area/effarea/detectors/detectors.py
@@ -24,6 +24,19 @@ class DetectorSelection:
             sc_pos=self._position_interpolator.sc_pos(0) * u.km,
         )
 
+    def _set_gbm_frame(self):
+        quats = self._position_interpolator.quaternion(0)
+        sc_pos = self._position_interpolator.sc_pos(0) * u.km
+        self._gbm_frame = GBMFrame(
+            quaternion_1=quats[0],
+            quaternion_2=quats[1],
+            quaternion_3=quats[2],
+            quaternion_4=quats[3],
+            sc_pos_X=sc_pos[0],
+            sc_pos_Y=sc_pos[1],
+            sc_pos_Z=sc_pos[2],
+        )
+
     @property
     def gbm(self):
         return self._gbm
