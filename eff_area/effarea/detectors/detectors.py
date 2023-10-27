7b1e0aa9473f8ef3c506b111797fd4ec27f8de1c
PreisTo <tpreis@mpe.mpg.de>
2023-10-27 11:06:54 +0200

    set normalizing det

--- eff_area/effarea/detectors/detectors.py
+++ eff_area/effarea/detectors/detectors.py
@@ -6,12 +6,53 @@ from gbmgeometry.gbm_frame import GBMFrame
 from gbmgeometry.gbm import GBM
 import astropy.units as u
 
+lu = [
+    "n0",
+    "n1",
+    "n2",
+    "n3",
+    "n4",
+    "n5",
+    "n6",
+    "n7",
+    "n8",
+    "n9",
+    "na",
+    "nb",
+    "b0",
+    "b1",
+]
+
 
 class DetectorSelection:
-    def __init__(self, grb):
+    def __init__(self, grb, max_sep=60, max_sep_normalizing=20):
         assert type(grb) is GRB, "grb needs to be an GRB object"
+        self.grb = grb
+        self._max_sep = max_sep
+        self._max_sep_normalizing = max_sep_normalizing
         self._set_position_interpolator()
         self._set_gbm()
+        self._set_good_dets()
+        self._set_normalizing_det()
+
+    def _set_good_dets(self):
+        self._good_dets = self._gbm.get_good_detectors(self.grb.position, self._max_sep)
+
+    def _set_normalizing_det(self):
+        self._separations = {}
+        seps = self.gbm.get_separation(self.grb.position)
+        min_sep = 180
+        min_sep_det = ""
+        for d in self._good_dets:
+            self._separations[d] = seps[d]
+            if seps[d] < 180:
+                min_sep = seps[d]
+                min_sep_det = d
+
+        if min_sep > self._max_sep_normalizing:
+            raise DetectorSelectionError
+        else:
+            self._normalizing_det = min_sep_det
 
     def _set_position_interpolator(self):
         self._position_interpolator = PositionInterpolator.from_trigdat(
