649b836a2ef7c408c4aefb37868a499aec21ed67
PreisTo <tpreis@mpe.mpg.de>
2023-10-30 14:48:34 +0100

    lines indicating det side

--- effarea/io/plot_dest_dist.py
+++ effarea/io/plot_dest_dist.py
@@ -8,15 +8,22 @@ class DetDistPlot:
     def __init__(self, result_path="/home/tobi/Schreibtisch/localizing/det_matrix.npy"):
         self._matrix = np.load(result_path, allow_pickle=True)
 
-        fig, ax = plt.subplots(1, figsize=(15, 15))
-        cax = fig.add_axes([0.8, 0.1, 0.15, 0.6])
+        fig, ax = plt.subplots(1, figsize=(20, 20))
+        cax = fig.add_axes([0.1, 0.9, 0.6, 0.05])
         matrix = self._matrix[:, :, 0]
+        print(matrix)
         blank = np.empty((12, 12))
         for i in range(12):
             for j in range(12):
                 blank[i, j] = np.nan_to_num(np.mean(matrix[i, j]))
-        im = ax.imshow(blank, cmap="plasma")
+        print(blank)
+        im = ax.imshow(blank, cmap="coolwarm")
         for (j, i), label in np.ndenumerate(blank):
             ax.text(i, j, round(float(label), 3), ha="center", va="center")
-        fig.colorbar(im, cax=cax)
+        ax.axvline(x=5.5, color="black")
+        ax.axhline(y=5.5, color="black")
+        ax.set_title(
+            "Rel. Normalization of dets - max angle 60deg, max angle norm det 20deg"
+        )
+        fig.colorbar(im, cax=cax, orientation="horizontal")
         fig.savefig("/home/tobi/Schreibtisch/test.pdf")
