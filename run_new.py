082b698787c15490faea613ecf32e4a5ab387b5a
Tobias Preis <tpreis@necromancer.mpe.mpg.de>
2023-11-02 17:07:25 +0100

    no bare except

--- run_new.py
+++ run_new.py
@@ -3,14 +3,17 @@
 from fierywhip.data.grbs import GRB, GRBList
 from fierywhip.model.model import GRBModel
 from fierywhip.io.export import Exporter
+from threeML.minimizer.minimization import FitFailed
+
 if __name__ == "__main__":
     grb_list = GRBList()
     for grb in grb_list.grbs:
+        print(f"Started for {grb.name}\n\n")
         try:
             model = GRBModel(grb)
             exporter = Exporter(model)
             exporter.export_yaml()
             exporter.export_matrix()
-        except Exception as e:
+        except (FitFailed,TypeError) as e:
             print(e)
 
