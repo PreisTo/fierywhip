7496da00bdcb7235efd14170da8bf9c7c3485a74
PreisTo <tpreis@mpe.mpg.de>
2023-11-07 22:37:49 +0100

    helper file creating det dist plot

---
+++ matrix_plot.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from fierywhip.io.export import matrix_from_yaml
+from fierywhip.io.plot_dest_dist import DetDistPlot
+
+if __name__ == "__main__":
+    excludes = [
+        "GRB230812790",
+        "GRB220310933",
+        "GRB210119121",
+        "GRB190123513",
+        "GRB190515190",
+        "GRB200109074",
+        "GRB200528436",
+        "GRB210308276",
+        "GRB211129410",
+    ]
+
+    print(f"These GRBs are excluded: {excludes}")
+
+    matrix = matrix_from_yaml(
+        "/home/tobi/Schreibtisch/localizing/results.yml", exclude=excludes
+    )
+    plot = DetDistPlot(matrix=matrix)
