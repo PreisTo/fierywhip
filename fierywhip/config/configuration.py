13785709e8e9b8ca437f69e87a84649467cf94df
PreisTo <tpreis@mpe.mpg.de>
2023-11-08 23:07:12 +0100

    init conifg

---
+++ fierywhip/config/configuration.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from omegaconf import OmegaConf
+
+structure = {}
+structure["eff_corr_lims"] = (0.8, 1.2)
+structure["live_points"] = 800
+structure["live_points_trigdat"] = 1200
+strucutre["max_sep"] = 60
+structure["max_sep_norm"] = 40
+structure["max_number_det"] = 4
+structure["min_number_det"] = 3
+fierywhip_config = OmegaConf.create(structure)
