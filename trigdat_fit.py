c37d3dba71f9fd3d19badb86c05a2b9f56a981ee
PreisTo <tpreis@mpe.mpg.de>
2023-11-08 17:07:07 +0100

    mpi

--- trigdat_fit.py
+++ trigdat_fit.py
@@ -5,11 +5,20 @@ from fierywhip.data.grbs import GRB, GRBList
 from fierywhip.model.trigdat import GRBModel
 from fierywhip.normalizations.normalization_matrix import NormalizationMatrix
 import os
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
 if __name__ == "__main__":
-    norm_matrix = NormalizationMatrix(
-        result_yml=os.path.join(os.environ.get("GBMDATA"), "localizing/results.yml")
-    )
+    if rank == 0:
+        norm_matrix = NormalizationMatrix(
+            result_yml=os.path.join(os.environ.get("GBMDATA"), "localizing/results.yml")
+        )
+    else:
+        norm_matrix = None
+    norm_matrix = comm.Bcast(norm_matrix, root=0)
     grb_list = GRBList(check_finished=False)
 
     for grb in grb_list.grbs:
