2b13c0751f272f4fc525bf7d779a86c5b131080e
PreisTo <tpreis@mpe.mpg.de>
2023-11-24 14:30:49 +0100

    rel selection plot

--- fierywhip/io/plot_dest_dist.py
+++ fierywhip/io/plot_dest_dist.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 from fierywhip.config.configuration import fierywhip_config
 import os
 
@@ -16,11 +17,21 @@ class DetDistPlot:
         ),
         matrix=None,
         lims=(fierywhip_config.eff_corr_lim_low, fierywhip_config.eff_corr_lim_high),
+        selection_plot=True,
     ):
         if matrix is None:
             self._matrix = np.load(result_path, allow_pickle=True)
         else:
             self._matrix = matrix
+        self._lims = lims
+        self._make_selection_plot = selection_plot
+        self._eff_area_correction_plot()
+        self._selection_plot()
+
+    def _eff_area_correction_plot(self):
+        # TODO use normalization/normalization_matrix
+        if self._make_selection_plot:
+            self._selection_matrix = np.zeros((12, 12), dtype=np.float64)
         fig, ax = plt.subplots(1, figsize=(20, 21))
         matrix = self._matrix[:, :, 0]
         error_pos = np.array(self._matrix[:, :, 1])
@@ -34,12 +45,13 @@ class DetDistPlot:
                     if i != j:
                         ep = error_pos[i, j]
                         en = error_neg[i, j]
-                        if lims is not None:
+                        if self._lims is not None:
                             pop_indices = []
                             for ind in range(len(matrix[i, j])):
                                 if (
-                                    np.abs(matrix[i, j][ind] / lims[0] - 1) < 0.1
-                                    or np.abs(matrix[i, j][ind] / lims[1] + 1) < 0.1
+                                    np.abs(matrix[i, j][ind] / self._lims[0] - 1) < 0.1
+                                    or np.abs(matrix[i, j][ind] / self._lims[1] + 1)
+                                    < 0.1
                                 ):
                                     if (np.abs(en[ind]) + np.abs(en[ind])) < 0.1:
                                         pop_indices.append(ind)
@@ -77,6 +89,13 @@ class DetDistPlot:
         for index, label in np.ndenumerate(blank):
             i = index[0]
             j = index[1]
+            if self._make_selection_plot:
+                if f"{i},{j}" in self._rejected.keys():
+                    self._selection_matrix[i, j] = (
+                        len(self._matrix[i, j, 0]) - self._rejected[f"{i},{j}"]
+                    )
+                else:
+                    self._selection_matrix[i, j] = len(self._matrix[i, j, 0])
             if str(label) != "nan":
                 try:
                     label = (
@@ -101,3 +120,35 @@ class DetDistPlot:
         fig.colorbar(im, cax=cax, orientation="horizontal")
         fig.savefig(os.path.join(fierywhip_config.default_plot_path, "test.pdf"))
         print(self._rejected)
+
+    def _selection_plot(self):
+        fig, ax = plt.subplots(1, figsize=(20, 21))
+        matrix = self._selection_matrix.copy().astype(np.float64)
+        total_bursts = np.trace(matrix)
+        matrix = matrix / total_bursts * 100
+        np.fill_diagonal(matrix, np.nan)
+        im = ax.imshow(matrix, cmap="coolwarm")
+        for i in range(12):
+            for j in range(12):
+                ax.text(
+                    j,
+                    i,
+                    round(self._selection_matrix[i, j] / total_bursts * 100, 3),
+                    ha="center",
+                    va="center",
+                )
+        ax.set_ylabel("Normalizing Det")
+        ax.set_xlabel("Detector")
+        ax.set_xticks(range(12), labels=lu)
+        ax.set_yticks(range(12), labels=lu)
+        ax.set_title(
+            f"Relative Selection of dets for {total_bursts} GRBs\n Mode: {fierywhip_config.det_sel.mode}"
+        )
+        fig.tight_layout()
+
+        plt.subplots_adjust(bottom=0.2, top=0.8)
+        cax = fig.add_axes([0.2, 0.1, 0.6, 0.05])
+        fig.colorbar(im, cax=cax, orientation="horizontal")
+        fig.savefig(
+            os.path.join(fierywhip_config.default_plot_path, "test_selection.pdf")
+        )
