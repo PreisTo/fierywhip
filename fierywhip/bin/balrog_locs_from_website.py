48dc78ba4eccfc54cfda5f24ad9475f8c28bfc90
PreisTo <tpreis@mpe.mpg.de>
2023-11-27 07:08:58 +0100

    check if json exists

--- fierywhip/bin/balrog_locs_from_website.py
+++ fierywhip/bin/balrog_locs_from_website.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
     grb_list = GRBList(run_det_sel=False)
     for grb in grb_list.grbs:
         bl = BalrogLocalization(grb, result_df)
-        bl.add_row_df()
+        if bl.exists:
+            bl.add_row_df()
     save_df(result_df)
