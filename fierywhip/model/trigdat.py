510097849946f0331bed463b27bbcc95b5eeb610
PreisTo <tpreis@mpe.mpg.de>
2023-11-28 07:36:14 +0100

    black

--- fierywhip/model/trigdat.py
+++ fierywhip/model/trigdat.py
@@ -117,7 +117,10 @@ class GRBModel:
 
         self._bayes.set_sampler("multinest", share_spectrum=True)
         self._bayes.sampler.setup(
-            n_live_points=fierywhip_config.live_points_trigdat, chain_name=chain_path, wrapped_params=wrap, verbose=True
+            n_live_points=fierywhip_config.live_points_trigdat,
+            chain_name=chain_path,
+            wrapped_params=wrap,
+            verbose=True,
         )
         comm.Barrier()
         self._bayes.sample()
