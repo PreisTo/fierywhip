061fdf14735b32d6e1a89130dd9dc45b7eb977a8
PreisTo <tpreis@mpe.mpg.de>
2023-11-28 12:04:42 +0100

    fix check if already run

--- fierywhip/bin/run_default_balrog.py
+++ fierywhip/bin/run_default_balrog.py
@@ -6,20 +6,23 @@ import pandas as pd
 import os
 
 if __name__ == "__main__":
-    try:
+    if os.path.exists(
+        os.path.join(os.environ.get("GBM_TRIGGER_DATA_DIR"), "morgoth_results.csv")
+    ):
         already_run = pd.read_csv(
             os.path.join(os.environ.get("GBM_TRIGGER_DATA_DIR"), "morgoth_results.csv"),
-            index_col=None,
         )
-    except FileNotFoundError:
+    else:
         already_run = None
     grb_list = GRBList(run_det_sel=False, check_finished=False, testing=50)
     for g in grb_list.grbs:
         print(f"Checking {g.name}")
         if already_run is not None:
-            if g.name not in already_run["grb"]:
+            if g.name not in list(already_run["grb"]):
                 print(f"Starting Morgoth for {g.name}")
                 rm = RunMorgoth(g)
+            else:
+                print(f"Skipping Morgoth for {g.name}")
         else:
             print(f"Starting Morgoth for {g.name}")
             rm = RunMorgoth(g)
