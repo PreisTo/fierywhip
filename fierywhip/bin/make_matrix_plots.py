277f8f992bb130e0acf1d2592a1642bd755a6448
PreisTo <tpreis@mpe.mpg.de>
2023-11-28 12:02:08 +0100

    no excuses...ludes

--- fierywhip/bin/make_matrix_plots.py
+++ fierywhip/bin/make_matrix_plots.py
@@ -6,17 +6,6 @@ from fierywhip.config.configuration import fierywhip_config
 import os
 
 if __name__ == "__main__":
-    excludes = [
-        "GRB230812790",
-        "GRB220310933",
-        "GRB210119121",
-        "GRB190123513",
-        "GRB190515190",
-        "GRB200109074",
-        "GRB200528436",
-        "GRB210308276",
-        "GRB211129410",
-    ]
     excludes = []
     print(f"These GRBs are excluded: {excludes}")
 
