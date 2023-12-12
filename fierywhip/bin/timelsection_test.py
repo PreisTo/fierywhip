2b6449df36bda4806171263e5ee217831e315b3f
PreisTo <tpreis@mpe.mpg.de>
2023-12-12 17:31:23 +0100

    tester for timeselection

---
+++ fierywhip/bin/timelsection_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from fierywhip.frameworks.grbs import GRBList, GRB
+from fierywhip.config.configuration import fierywhip_config
+import yaml
+import os
+
+if __name__ == "__main__":
+    fierywhip_config.timeselection.store_and_reload = False
+    yaml_path = os.path.join(os.environ.get("HOME"), "ts.yml")
+    for g in grblist.grbs:
+        g.run_timeselection()
+        active_time = g.active_time
+        bkg_time = g.bkg_time
+        res = {}
+        res["active_time"] = active_time
+        res["bkg_neg"] = bkg_time[0]
+        res["bkg_pos"] = bkg_time[1]
+        try:
+            with open(yaml_path, "r") as f:
+                file_content = yaml.safe_load(f)
+                file_content[g.name] = res
+        except FileNotFoundError:
+            file_content = {g.name: res}
+            with open(yaml_path, "w+") as f:
+                yaml.safe_dump(file_content, f)
