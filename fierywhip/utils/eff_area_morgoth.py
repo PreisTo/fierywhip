83bbe1ef1d457d587615e9b529e948c750adcdb3
PreisTo <tpreis@mpe.mpg.de>
2024-01-16 16:37:07 +0100

    class for multiple responses

--- fierywhip/utils/eff_area_morgoth.py
+++ fierywhip/utils/eff_area_morgoth.py
@@ -223,3 +223,246 @@ class MultinestFitTrigdatEffArea(MultinestFitTrigdat):
                 )
             trig_data.append(balrog_like)
         self._data_list = DataList(*trig_data)
+
+
+class MultinestFitTrigdatMultipleSelections(MultinestFitTrigdatEffArea):
+    def __init__(
+        self,
+        grb: GRB,
+        grb_name: str,
+        version: str,
+        trigdat_file: str,
+        bkg_fit_yaml_file: str,
+        time_selection_yaml_file: str,
+        use_eff_area: bool = False,
+        det_sel_mode: str = "default",
+        grb_file: str = None,
+        **kwargs,
+    ):
+        super().__init__(
+            self,
+            grb,
+            grb_name,
+            version,
+            trigdat_file,
+            bkg_fit_yaml_file,
+            time_selection_yaml_file,
+            use_eff_area,
+            det_sel_mode,
+            grb_file,
+            **kwargs,
+        )
+
+    def setup_essentials(self):
+        with open(self._bkg_fit_yaml_file, "r") as f:
+            data = yaml.safe_load(f)
+            self._bkg_fit_yaml_file = data["bkg_fit_files"]
+
+        with open(self._time_selection_yaml_file, "r") as f:
+            data = yaml.safe_load(f)
+            self._active_time = (
+                f"{data['active_time']['start']}-{data['active_time']['stop']}"
+            )
+            self._fine = data["fine"]
+        # TODO determine lenght of duration here and decide how many models
+        # TODO THIS NEEDS TO BE DONE BEFORE DECIDING WHICH CLASS!!!!
+        active_time = self._active_time
+        ats = active_time.split("-")
+        if len(ats) == 2:
+            start = float(ats[0])
+            stop = float(ats[-1])
+        elif len(ats) == 3:
+            start = -float(ats[1])
+            stop = float(ats[-1])
+        elif len(ats) == 4:
+            start = -float(ats[1])
+            stop = -float(ats[-1])
+        else:
+            raise ValueError("Something wrong with active time")
+        self._active_times_float = [start, start + (stop - start) / 2, stop]
+        self._define_model(self._spectrum_model)
+        self._setup_plugins()
+
+    def _setup_plugins(self):
+        """
+        Set the plugins using the saved background hdf5 files
+        :return:
+        """
+        success_restore = False
+        i = 0
+        while not success_restore:
+            try:
+                trig_reader = TrigReader(
+                    self._trigdat_file,
+                    fine=self._fine,
+                    verbose=False,
+                    restore_poly_fit=self._bkg_fit_files,
+                )
+                success_restore = True
+                i = 0
+            except Exception as e:
+                import time
+
+                time.sleep(1)
+                pass
+            i += 1
+            if i == 50:
+                raise AssertionError(
+                    f"Can not restore background fit...\n{self._bkg_fit_files}"
+                )
+        logging.info(
+            f"Duration of Burst is {self._active_time_float[-1]-self._active_times_float[0]}, we will use 2 responses for this"
+        )
+
+        # First
+        trig_reader.set_active_time_interval(
+            f"{self._active_times_float[0]}-{self._active_times_float[1]}"
+        )
+        trig_data = []
+        for d in self._use_dets:
+            speclike = trig_reader.time_series[d].to_spectrumlike()
+            time = 0.5 * (
+                trig_reader.time_series[d].tstart + trig_reader.time_series[d].tstop
+            )
+            balrog_like = BALROGLike.from_spectrumlike(speclike, time=time)
+            balrog_like.assign_to_source("first")
+            balrog_like.set_active_measurements("c1-c6")
+            if self._use_eff_area:
+                balrog_like.fix_eff_area_correction(
+                    self._grb.effective_area_correction(d)
+                )
+            trig_data.append(balrog_like)
+
+        trig_reader.set_active_time_interval(
+            f"{self._active_times_float[1]}-{self._active_times_float[2]}"
+        )
+        for d in self._use_dets:
+            speclike = trig_reader.time_series[d].to_spectrumlike()
+            time = 0.5 * (
+                trig_reader.time_series[d].tstart + trig_reader.time_series[d].tstop
+            )
+            balrog_like = BALROGLike.from_spectrumlike(speclike, time=time)
+            balrog_like.assign_to_source("second")
+            balrog_like.set_active_measurements("c1-c6")
+            if self._use_eff_area:
+                balrog_like.fix_eff_area_correction(
+                    self._grb.effective_area_correction(d)
+                )
+            trig_data.append(balrog_like)
+
+        self._data_list = DataList(*trig_data)
+        # define bayes object with model and data_list
+        self._bayes = BayesianAnalysis(self._model, self._data_list)
+
+    def _define_model(self, spectrum="cpl"):
+        """
+        Define a Model for the fit
+        :param spectrum: Which spectrum type should be used (cpl, band, pl, sbpl or solar_flare)
+        """
+        # data_list=comm.bcast(data_list, root=0)
+        if spectrum == "cpl":
+            # we define the spectral model
+            cpl1 = Cutoff_powerlaw()
+            cpl1.K.max_value = 10**4
+            cpl1.K.prior = Log_uniform_prior(lower_bound=1e-3, upper_bound=10**4)
+            cpl1.xc.prior = Log_uniform_prior(lower_bound=1, upper_bound=1e4)
+            cpl1.index.set_uninformative_prior(Uniform_prior)
+            # we define a point source model using the spectrum we just specified
+            ps1 = PointSource("first", 0.0, 0.0, spectral_shape=cpl1)
+            cpl2 = Cutoff_powerlaw()
+            cpl2.K.max_value = 10**4
+            cpl2.K.prior = Log_uniform_prior(lower_bound=1e-3, upper_bound=10**4)
+            cpl2.xc.prior = Log_uniform_prior(lower_bound=1, upper_bound=1e4)
+            cpl2.index.set_uninformative_prior(Uniform_prior)
+            # we define a point source model using the spectrum we just specified
+            ps2 = PointSource("second", 0.0, 0.0, spectral_shape=cpl2)
+            self._model = Model(ps1, ps2)
+            self._model.link(
+                self._model.first.position.ra, self._model.second.position.ra
+            )
+            self._model.link(
+                self._model.first.positon.dec, self._model.second.position.dec
+            )
+        # elif spectrum == "band":
+        #     band = Band()
+        #     band.K.prior = Log_uniform_prior(lower_bound=1e-5, upper_bound=1200)
+        #     band.alpha.set_uninformative_prior(Uniform_prior)
+        #     band.xp.prior = Log_uniform_prior(lower_bound=10, upper_bound=1e4)
+        #     band.beta.set_uninformative_prior(Uniform_prior)
+
+        #     self._model = Model(PointSource("GRB_band", 0.0, 0.0, spectral_shape=band))
+
+        # elif spectrum == "pl":
+        #     pl = Powerlaw()
+        #     pl.K.max_value = 10**4
+        #     pl.K.prior = Log_uniform_prior(lower_bound=1e-3, upper_bound=10**4)
+        #     pl.index.set_uninformative_prior(Uniform_prior)
+        #     # we define a point source model using the spectrum we just specified
+        #     self._model = Model(PointSource("GRB_pl", 0.0, 0.0, spectral_shape=pl))
+
+        # elif spectrum == "sbpl":
+        #     sbpl = SmoothlyBrokenPowerLaw()
+        #     sbpl.K.min_value = 1e-5
+        #     sbpl.K.max_value = 1e4
+        #     sbpl.K.prior = Log_uniform_prior(lower_bound=1e-5, upper_bound=1e4)
+        #     sbpl.alpha.set_uninformative_prior(Uniform_prior)
+        #     sbpl.beta.set_uninformative_prior(Uniform_prior)
+        #     sbpl.break_energy.min_value = 1
+        #     sbpl.break_energy.prior = Log_uniform_prior(lower_bound=1, upper_bound=1e4)
+        #     self._model = Model(PointSource("GRB_sbpl", 0.0, 0.0, spectral_shape=sbpl))
+
+        # elif spectrum == "solar_flare":
+        #     # broken powerlaw
+        #     bpl = Broken_powerlaw()
+        #     bpl.K.max_value = 10**5
+        #     bpl.K.prior = Log_uniform_prior(lower_bound=1e-3, upper_bound=10**5)
+        #     bpl.xb.prior = Log_uniform_prior(lower_bound=1, upper_bound=1e4)
+        #     bpl.alpha.set_uninformative_prior(Uniform_prior)
+        #     bpl.beta.set_uninformative_prior(Uniform_prior)
+
+        #     # thermal brems
+        #     tb = Thermal_bremsstrahlung_optical_thin()
+        #     tb.K.max_value = 1e5
+        #     tb.K.min_value = 1e-5
+        #     tb.K.prior = Log_uniform_prior(lower_bound=1e-5, upper_bound=10**5)
+        #     tb.kT.prior = Log_uniform_prior(lower_bound=1e-3, upper_bound=1e4)
+        #     tb.Epiv.prior = Log_uniform_prior(lower_bound=1e-3, upper_bound=1e4)
+
+        #     # combined
+        #     total = bpl + tb
+
+        #     self._model = Model(
+        #         PointSource("Solar_flare", 0.0, 0.0, spectral_shape=total)
+        #     )
+        else:
+            raise Exception("Use valid model type: cpl, pl, sbpl, band or solar_flare")
+
+    def fit(self):
+        """
+        Fit the model to data using multinest
+        :return:
+        """
+
+        self._bayes = BayesianAnalysis(self._model, self._data_list)
+        # wrap for ra angle
+        wrap = [0] * len(self._model.free_parameters)
+        wrap[0] = 1
+
+        # define temp chain save path
+        self._temp_chains_dir = os.path.join(
+            base_dir, self._grb_name, f"c_trig_{self._version}"
+        )
+        chain_path = os.path.join(self._temp_chains_dir, f"trigdat_{self._version}_")
+
+        # Make temp chains folder if it does not exists already
+        if not os.path.exists(self._temp_chains_dir):
+            os.mkdir(os.path.join(self._temp_chains_dir))
+
+        # use multinest to sample the posterior
+        # set main_path+trigger to whatever you want to use
+
+        self._bayes.set_sampler("multinest", share_spectrum=True)
+        self._bayes.sampler.setup(
+            n_live_points=800, chain_name=chain_path, wrapped_params=wrap, verbose=True
+        )
+        self._bayes.sample()
