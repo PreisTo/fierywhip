2b7b2002fdc0a2ff3a96059ddec1647af67926c6
PreisTo <tpreis@mpe.mpg.de>
2024-02-01 11:29:25 +0100

    bug fixes

--- fierywhip/bin/create_healpix.py
+++ fierywhip/bin/create_healpix.py
@@ -8,6 +8,8 @@ import logging
 
 def run(path, output_path):
     ls = os.listdir(path)
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
     for l in ls:
         if re.search("GRB[0-9]{9}", l) is not None and os.path.isdir(
             os.path.join(path, l)
@@ -18,10 +20,10 @@ def run(path, output_path):
                 path, l, "trigdat", "v00", "trigdat_v00_loc_results.fits"
             )
             hp = MorgothHealpix.from_result_fits_file(result_file=result_file)
-            hp.save_healpix_map(path=os.path.join(output_path, f"grb.fits"))
+            hp.save_healpix_map(path=os.path.join(output_path, f"{grb}.fits"))
 
 
 if __name__ == "__main__":
-    path = "/data/tpreis/test_morgoth/website_version/trigger_data_dir/GBM_TRIGGER_DATA_DIR"
+    path = "/data/tpreis/test_morgoth/website_version/trigger_data_dir/GBM_TRIGGER_DATA"
     output_path = "/data/tpreis/test_morgoth/website_version/healpix/"
     run(path, output_path)
