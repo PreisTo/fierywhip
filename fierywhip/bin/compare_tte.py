230540070170e8dbbb5aaf3a04780f5067758a94
PreisTo <tpreis@mpe.mpg.de>
2024-04-19 17:41:49 +0200

    hardcoded eff area corrections

--- fierywhip/bin/compare_tte.py
+++ fierywhip/bin/compare_tte.py
@@ -20,9 +20,13 @@ size = comm.Get_size()
 
 class CustomEffAreaCorrections(GRBModel):
 
-    def __init__(self, eff_area_dict=None):
+    def __init__(self,grb,fix_position=False,use_eff_area=True,
+        base_dir=None,
+        eff_area_dict=None,
+        smart_ra_dec=False):
         self._eff_area_dict = eff_area_dict
-        super().__init__()
+        super().__init__(grb,fix_position=fix_position,use_eff_area=use_eff_area,base_dir=base_dir,smart_ra_dec=smart_ra_dec)
+
 
     def _to_plugin(self):
         if self._fix_position:
@@ -79,7 +83,17 @@ if __name__ == "__main__":
     name = "GRB130216790"  # "GRB190824616"
     ra = 58.875000  # 215.329
     dec = 2.033000  # -41.90
-    eff_area_dict = {"n7": 1.1, "n8": 0.92, "nb": 0.9}
+    eff_area_dict = {"n0":1,"n1": 0.9929363050000001,
+             "n2": 0.9514813730000001,
+              "n3": 1.0400485800000001,
+               "n4": 0.9644063780000001,
+                "n5": 1.06590465,
+                 "n6": 0.979424989,
+                  "n7": 1.1175595400000002,
+                   "n8": 0.924959813,
+                    "n9": 0.94208006,
+                     "na": 0.9274396269999999,
+                      "nb": 0.902805862}
     timeselection_path = os.path.join(
         "/data/tpeis/test_morgoth/triplets_standard/GBM_TRIGGER_DATA",
         name,
@@ -111,11 +125,11 @@ if __name__ == "__main__":
         f"{name}.fits",
     )
 
-    # if not os.path.exists(res_file):
-    #    model.fit()
-    #    res = model.results
-    # else:
-    res = load_analysis_results(res_file)
+    #if not os.path.exists(res_file):
+    model.fit()
+    res = model.results
+    #else:
+    #    res = load_analysis_results(res_file)
 
     model_eff = CustomEffAreaCorrections(
         grb,
@@ -136,21 +150,22 @@ if __name__ == "__main__":
         name,
         f"{name}.fits",
     )
-    # if not os.path.exists(res_file):
-    #    model_eff.fit()
-    #    res_eff = model_eff.results
-    # else:
-    res_eff = load_analysis_results(res_file_eff)
+    #if not os.path.exists(res_file):
+    model_eff.fit()
+    res_eff = model_eff.results
+    #else:
+    #    res_eff = load_analysis_results(res_file_eff)
+    comm.Barrier()
+    if rank == 0:
+        res.display()
+        res_eff.display()
 
-    res.display()
-    res_eff.display()
-
-    true_grb = SkyCoord(ra=ra, dec=dec, unit=(u.deg, u.deg))
-    for x in [res, res_eff]:
-        ra_fit = x.get_data_frame().loc["GRB.position.ra"]["value"]
-        dec_fit = x.get_data_frame().loc["GRB.position.dec"]["value"]
-        print(
-            true_grb.separation(
-                SkyCoord(ra=ra_fit, dec=dec_fit, unit=(u.deg, u.deg))
-            ).deg
-        )
+        true_grb = SkyCoord(ra=ra, dec=dec, unit=(u.deg, u.deg))
+        for x in [res, res_eff]:
+            ra_fit = x.get_data_frame().loc["GRB.position.ra"]["value"]
+            dec_fit = x.get_data_frame().loc["GRB.position.dec"]["value"]
+            print(
+                true_grb.separation(
+                    SkyCoord(ra=ra_fit, dec=dec_fit, unit=(u.deg, u.deg))
+                ).deg
+            )
