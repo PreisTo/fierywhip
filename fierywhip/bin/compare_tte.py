03681017a91fd6b53ee90bfafe0a2735f8f41e99
PreisTo <19503558+PreisTo@users.noreply.github.com>
2024-04-19 15:20:35 +0200

    fix eff area

--- fierywhip/bin/compare_tte.py
+++ fierywhip/bin/compare_tte.py
@@ -2,28 +2,88 @@
 
 from fierywhip.frameworks.grbs import GRB
 from fierywhip.model.model import GRBModel
+from fierywhip.config.configuration import fierywhip_config
 from threeML.analysis_results import load_analysis_results
 from mpi4py import MPI
 from astropy.coordinates import SkyCoord
 import astropy.units as u
 import os
+import logging
+from gbm_drm_gen.io.balrog_like import BALROGLike
+from threeML.data_list import DataList
+
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
+
 class CustomEffAreaCorrections(GRBModel):
 
-    def __init__(self):
+    def __init__(self, eff_area_dict=None):
+        self._eff_area_dict = eff_area_dict
         super().__init__()
-        
+
+    def _to_plugin(self):
+        if self._fix_position:
+            free_position = False
+        else:
+            free_position = True
+        active_time = self.grb.active_time
+        active_time = active_time.split("-")
+        if len(active_time) == 2:
+            start = float(active_time[0])
+            stop = float(active_time[-1])
+        if len(active_time) == 3:
+            start = -float(active_time[1])
+            stop = float(active_time[-1])
+        elif len(active_time) == 4:
+            start = -float(active_time[1])
+            stop = -float(active_time[-1])
+        assert start < stop, "start is after stop"
+        response_time = (float(start) + float(stop)) / 2
+        spectrum_likes = []
+        for d in self.grb.detector_selection.good_dets:
+            if self._timeseries[d]._name not in ("b0", "b1"):
+                spectrum_like = self._timeseries[d].to_spectrumlike()
+                spectrum_like.set_active_measurements("10-500")
+                spectrum_likes.append(spectrum_like)
+            else:
+                spectrum_like = self._timeseries[d].to_spectrumlike()
+                spectrum_like.set_active_measurements("300-30000")
+                spectrum_likes.append(spectrum_like)
+        balrog_likes = []
+        logging.info(f"We are going to use {self.grb.detector_selection.good_dets}")
+        for i, d in enumerate(self.grb.detector_selection.good_dets):
+            if d not in ("b0", "b1"):
+                bl = BALROGLike.from_spectrumlike(
+                    spectrum_likes[i],
+                    response_time,
+                    self._responses[d],
+                    free_position=free_position,
+                )
+                if self._use_eff_area:
+                    bl.fix_effective_area_correction(self._eff_area_dict[d])
+                balrog_likes.append(bl)
+        self._data_list = DataList(*balrog_likes)
+        if self._save_lc:
+            for d in self.grb.detector_selection.good_dets:
+                fig = self._timeseries[d].view_lightcurve()
+                plot_path = os.path.join(self._base_dir, self.grb.name, "lightcurves/")
+                if not os.path.exists(plot_path):
+                    os.makedirs(plot_path)
+                fig.savefig(os.path.join(plot_path, f"{d}.pdf"))
+
 
 if __name__ == "__main__":
-    name = "GRB130216790" #"GRB190824616" 
-    ra = 58.875000 #215.329
-    dec = 2.033000 #-41.90
-
-    timeselection_path = os.path.join("/data/tpeis/test_morgoth/triplets_standard/GBM_TRIGGER_DATA", name, "timeselection.yml"
+    name = "GRB130216790"  # "GRB190824616"
+    ra = 58.875000  # 215.329
+    dec = 2.033000  # -41.90
+    eff_area_dict = {"n7": 1.1, "n8": 0.92, "nb": 0.9}
+    timeselection_path = os.path.join(
+        "/data/tpeis/test_morgoth/triplets_standard/GBM_TRIGGER_DATA",
+        name,
+        "timeselection.yml",
     )
 
     grb = GRB(name=name, ra=ra, dec=dec)
@@ -41,36 +101,52 @@ if __name__ == "__main__":
         ),
         smart_ra_dec=False,
     )
-    res_file = os.path.join(os.environ.get("GBMDATA"),"localizing","comparison",name,"no_eff",name,f"{name}.fits")
+    res_file = os.path.join(
+        os.environ.get("GBMDATA"),
+        "localizing",
+        "comparison",
+        name,
+        "no_eff",
+        name,
+        f"{name}.fits",
+    )
 
-    #if not os.path.exists(res_file):
+    # if not os.path.exists(res_file):
     #    model.fit()
     #    res = model.results
-    #else:
-    res = load_analysis_results(res_file) 
+    # else:
+    res = load_analysis_results(res_file)
 
-
-    model_eff = GRBModel(
+    model_eff = CustomEffAreaCorrections(
         grb,
         fix_position=False,
         use_eff_area=True,
         base_dir=os.path.join(
             os.environ.get("GBMDATA"), "localizing", "comparison", name, "eff"
         ),
+        eff_area_dict=eff_area_dict,
         smart_ra_dec=False,
     )
-    res_file_eff = os.path.join(os.environ.get("GBMDATA"),"localizing","comparison",name,"eff",name,f"{name}.fits")
-    #if not os.path.exists(res_file):
+    res_file_eff = os.path.join(
+        os.environ.get("GBMDATA"),
+        "localizing",
+        "comparison",
+        name,
+        "eff",
+        name,
+        f"{name}.fits",
+    )
+    # if not os.path.exists(res_file):
     #    model_eff.fit()
     #    res_eff = model_eff.results
-    #else:
-    res_eff = load_analysis_results(res_file_eff) 
+    # else:
+    res_eff = load_analysis_results(res_file_eff)
 
     res.display()
     res_eff.display()
 
     true_grb = SkyCoord(ra=ra, dec=dec, unit=(u.deg, u.deg))
-    for x in [res,res_eff]:
+    for x in [res, res_eff]:
         ra_fit = x.get_data_frame().loc["GRB.position.ra"]["value"]
         dec_fit = x.get_data_frame().loc["GRB.position.dec"]["value"]
         print(
